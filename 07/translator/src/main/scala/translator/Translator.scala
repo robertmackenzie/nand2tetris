83b3459ead48ae89471accd70c4950c13d217fee
Robert Mackenzie <robertmackenzie@gmail.com>
2017-12-02 16:12:00 +0000

    Function scope memory access commands translation.
    * Memory segments can be global scope or function scope.
    * All function scoped memory segments require similar translations.
    * Types GlobalSegment and FunctionSegment make scope explicit.
    * Explicit modelling of scope enables generic translation of function
      scoped memory access commands.

--- 07/translator/src/main/scala/translator/Translator.scala
+++ 07/translator/src/main/scala/translator/Translator.scala
@@ -5,15 +5,20 @@ import scala.io.Source
 sealed trait Direction
 case object Push extends Direction
 case object Pop extends Direction
+
 sealed trait Segment
-case object Constant extends Segment
-case object Local extends Segment
-case object Argument extends Segment
-case object This extends Segment
-case object That extends Segment
-case object Temp extends Segment
-case object Pointer extends Segment
-case object Static extends Segment
+sealed trait GlobalSegment extends Segment
+sealed trait FunctionSegment extends Segment
+
+case object Local extends FunctionSegment
+case object Argument extends FunctionSegment
+case object This extends FunctionSegment
+case object That extends FunctionSegment
+case object Constant extends GlobalSegment
+case object Temp extends GlobalSegment
+case object Pointer extends GlobalSegment
+case object Static extends GlobalSegment
+
 sealed trait VMStatement
 case class MemoryAccessCommand(direction: Direction, segment: Segment, index: Int) extends VMStatement
 sealed trait StackArithmeticCommand extends VMStatement
@@ -46,40 +51,77 @@ object Translator {
     }
   }
 
+  def toAsm(segment: Segment): String = segment match {
+    // SP?
+    case Local => "LCL"
+    case This => "THIS"
+    case That => "THAT"
+    case Argument => "ARG"
+    // ... GlobalSegment
+  }
+
+  def toVM(segment: Segment): String = segment match {
+    case Local => "local"
+    case This => "this"
+    case That => "that"
+    case Argument => "argument"
+    // ... GlobalSegment
+  }
+
   def toAsm(statement: VMStatement): String = statement match {
     case Add => ""
     case Sub => ""
-    case MemoryAccessCommand(Push, segment, i) => segment match {
-      case Argument => ""
-      case This => ""
-      case That => ""
-      case Temp => ""
-      case Local => ""
-      case Static => ""
-      case Constant => {
+    case MemoryAccessCommand(Push, segment: FunctionSegment, i) =>
+      s"""
+        |// push ${toVM(segment)} $i
+        |@$i
+        |D=A
+        |@${toAsm(segment)}
+        |A=M+D
+        |D=M
+        |@SP
+        |A=M
+        |M=D
+        |@SP
+        |M=M+1
+      """.stripMargin
+    case MemoryAccessCommand(Push, segment: GlobalSegment, i) => segment match {
+      case Constant =>
         s"""
-          |// *SP = i
-          |@${i}
+          |// push ${toVM(segment)} $i
+          |@$i
           |D=A
           |@SP
           |A=M
           |M=D
-          |// SP++
           |@SP
           |M=M+1
         """.stripMargin
-      }
       case Pointer => ""
-    }
-    case MemoryAccessCommand(Pop, segment, i) => segment match {
-      case Argument => ""
-      case This => ""
-      case That => ""
       case Temp => ""
-      case Local => ""
       case Static => ""
+    }
+    case MemoryAccessCommand(Pop, segment: FunctionSegment, i) =>
+      s"""
+        |// pop ${toVM(segment)} $i
+        |@$i
+        |D=A
+        |@${toAsm(segment)}
+        |D=M+D
+        |@R13
+        |M=D
+        |@SP
+        |AM=M-1
+        |D=M
+        |@R13
+        |A=M
+        |M=D
+      """.stripMargin
+    case MemoryAccessCommand(Pop, segment: GlobalSegment, i) => segment match {
       case Constant => throw new MatchError("Cannot pop from Constant memory segment")
       case Pointer => ""
+      case Temp => ""
+      case Static => ""
     }
   }
 }
